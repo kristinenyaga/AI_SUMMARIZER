801ed669a2cee42cac1bf11902246eaa187d31af
krisine <kristine.nyaga@student.moringaschool.com>
2023-07-03 23:43:56 +0300

    define the screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,14 @@ export default {
     extend: {
       fontFamily: {
         satoshi: ["satoshi", "sans-serif"],
-        inter:["Inter","sans-serif"]
-      }
+        inter: ["Inter", "sans-serif"],
+      },
+      screens: {
+        sm: "350px", // Small devices (e.g., smartphones)
+        md: "768px", // Medium devices (e.g., tablets)
+        lg: "1024px", // Large devices (e.g., laptops)
+        xl: "1280px", // Extra-large devices (e.g., desktops)
+      },
     },
   },
   plugins: [],
